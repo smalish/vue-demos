1e0316b2c1164b14c4b43c6ec38ee732cfac80a1
yangying <1349365218@qq.com>
2017-08-25 13:54:06 +0800

    切换tab样式加入

--- firstVue/src/components/HomeFooter.vue
+++ firstVue/src/components/HomeFooter.vue
@@ -4,7 +4,7 @@
 	<!-- {selected: item.selected, item: true} -->
 		<div v-for="(item, index) in list" v-bind:class="[{selected:item.selected}, 'item']" v-bind:style="{width: 100/list.length + '%'}" v-on:click="changePage(index)">
 			<div class="item_div">
-				<img class="img" v-bind:src="item.imgSrc" >
+				<img class="img" v-bind:src="item.selected? item.imgSelected : item.imgSrc" >
 				<span class="text">{{item.name}}</span>
 			</div>
 		</div>
@@ -18,26 +18,31 @@
 				list: [{
 					name:'首页',
 					imgSrc:  require('../assets/img/footer/home.png'),
+					imgSelected: require('../assets/img/footer/userSelected.png'),
 					selected: true,
 					page: 'home'
 				},{
 					name:'附近',
 					imgSrc: require('../assets/img/footer/near.png'),
+					imgSelected: require('../assets/img/footer/userSelected.png'),
 					selected: false,
 					page: 'near'
 				},{
 					name:'逛一逛',
 					imgSrc: require('../assets/img/footer/shopping.png'),
+					imgSelected: require('../assets/img/footer/userSelected.png'),
 					selected: false,
 					page: 'near'
 				},{
 					name:'订单',
 					imgSrc: require('../assets/img/footer/order.png'),
+					imgSelected: require('../assets/img/footer/userSelected.png'),
 					selected: false,
 					page: 'near'
 				},{
 					name:'我的',
 					imgSrc: require('../assets/img/footer/user.png'),
+					imgSelected: require('../assets/img/footer/userSelected.png'),
 					selected: false,
 					page: 'near'
 				},]
