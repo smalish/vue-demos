87ee2138e44857f132f77d20ba121656b3a071d3
yangyingf <1349365218@qq.com>
2017-09-03 22:58:42 +0800

    逛一逛页面

--- firstVue/src/pages/shopping/ShoppingPage.vue
+++ firstVue/src/pages/shopping/ShoppingPage.vue
@@ -1,15 +1,20 @@
 <template>
 	<div class="shoppingPage">
-		<p>逛一逛</p>
-		<ul>
-			<li>精选 享美食 出去浪 约电影</li>
+		<ul class="header">
+			<li :class="['item', {selected: item.selected}]" v-for="item in tabData">{{item.name}}</li>
 		</ul>
 		<router-view></router-view>
 	</div>
 </template>
 
 <style scoped lang="less">
-	.shoppingPage{}
+	.shoppingPage{
+		.header{
+			display: block;width: 100%;list-style: none;border-bottom: 1px solid #e5e5e5;background-color: #fff;overflow: hidden;
+			.item{display: block;width: 25%;height: 4.2rem;line-height: 4.2rem;float: left;border-bottom: 2px solid #fff;font-size: 1.6rem;color: #666666;text-align: center;}
+			.selected{color: #06c1ae;border-bottom-color:#06c1ae;}
+		}
+	}
 
 </style>
 
@@ -18,7 +23,28 @@
 	export default{
 		data() {
 			return {
-
+				tabData:{
+					like: {
+						name: '精选',
+						selected: true,
+						list: []
+					},
+					food: {
+						name: '享美食',
+						selected: false,
+						list: []
+					},
+					out: {
+						name: '出去浪',
+						selected: false,
+						list: []
+					},
+					movie: {
+						name: '约电影',
+						selected: false,
+						list: []
+					}
+				}
 			}
 		},
 		components: {
